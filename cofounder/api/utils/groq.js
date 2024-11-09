e07fe84e23aae95af94e6735f328465396f02f34
Antonio Reid <read.musik@gmail.com>
2024-11-09 04:56:03 -0600

    refactor: Replace Groq and SambaNova clients with OpenAI client

--- cofounder/api/utils/groq.js
+++ cofounder/api/utils/groq.js
@@ -1,40 +1,40 @@
-import { Client } from "@groq/groq";
+import OpenAI from "openai";
 
-const client = new Client({
-  url: "https://api.groq.com",
-  token: process.env.GROQ_API_KEY,
+const client = new OpenAI({
+	url: "https://api.groq.com",
+	token: process.env.GROQ_API_KEY,
 });
 
 async function inference(options) {
-  const response = await client.query({
-    query: `query($input: String!) {
+	const response = await client.query({
+		query: `query($input: String!) {
       answer(question: $input) {
         answer
       }
     }`,
-    variables: { input: options.input },
-  });
-  return response.answer;
+		variables: { input: options.input },
+	});
+	return response.answer;
 }
 
 async function vectorize(options) {
-  const response = await client.query({
-    query: `query($input: String!) {
+	const response = await client.query({
+		query: `query($input: String!) {
       vectorize(text: $input) {
         vector
       }
     }`,
-    variables: { input: options.input },
-  });
-  return response.vectorize.vector;
+		variables: { input: options.input },
+	});
+	return response.vectorize.vector;
 }
 
 async function transcribe(options) {
-  throw new Error("Transcribe functionality is not implemented in this example");
+	throw new Error("Transcribe functionality is not implemented in this example");
 }
 
 export default {
-  inference,
-  vectorize,
-  transcribe,
+	inference,
+	vectorize,
+	transcribe,
 };
